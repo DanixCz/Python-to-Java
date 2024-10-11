c4fb54cda266f86a278c65979b376b2110f7e8df
Daniel Zeman <Daniel.Zeman@student.souepl.cz>
2024-10-11 11:32:29 +0200

    1

---
+++ Java.js
@@ -0,0 +1,11 @@
+let promena1 = 5;
+let promena2 = 3;
+
+function obs_kruh(t){
+    if(r>0){
+        return 3.14 * (r**2);
+    }
+    else{
+        return "Zadal jsi nulovou nebo zápornou hodnotu"
+    }   
+}
